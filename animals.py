9341b5214953ec187bdf5c81a21455699590afa0
SamuelDiai <diai.samuel@gmail.com>
2021-04-01 23:24:49 +0200

    .

--- animals.py
+++ animals.py
@@ -32,7 +32,7 @@ def animals(k, n_iter, alpha, beta):
         mapping[i] = animals_names[i, 0]
 
     G = nx.relabel_nodes(G, mapping)
-    fig = plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(6,6))
     nx.draw(G, with_labels=True, font_weight='bold')
     plt.title("Learned graph for the animal dataset k=%s n_iter=%s alpha=%.3f beta=%.3f" % (k , n_iter, alpha, beta))
     filename = os.path.join(plots_dir, 'animals', 'graph')
